b539a135159cb31d2d3611dc6d19d316bb138d05
lior gishry <liorgish@gmail.com>
2018-10-02 21:41:35 +0300

    more

--- server/routes/router.ts
+++ server/routes/router.ts
@@ -4,6 +4,10 @@ import {join} from "path";
 import * as fs from "fs";
 import {ComtactController} from "../controllers/contact.controller";
 import {CVController} from "../controllers/cv.controller";
+import {GalleryController} from "../controllers/gallery.controller";
+import {HomeController} from "../controllers/home.controller";
+import {LinksController} from "../controllers/links..controller";
+import {ProgramsController} from "../controllers/programs.controller";
 
 export class MyRouter {
 
@@ -17,6 +21,22 @@ export class MyRouter {
         router = Router();
         new CVController(router);
         app.use(`/api/cv`, router)
+
+        router = Router();
+        new GalleryController(router);
+        app.use(`/api/gallery`, router)
+
+        router = Router();
+        new HomeController(router);
+        app.use(`/api/home`, router)
+
+        router = Router();
+        new LinksController(router);
+        app.use(`/api/links`, router)
+
+        router = Router();
+        new ProgramsController(router);
+        app.use(`/api/programs`, router)
 
         // const readdir = promisify(fs.readdir);
         //
